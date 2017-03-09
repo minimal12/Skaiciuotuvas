671f0a40a124964489879cfaa283e84dd2f274c8
pvls <pvls>
2017-03-09 14:35:07 +0200

    Add some stuff, cause...

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,54 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		SimpleCalculations sc = new SimpleCalculations();
-		sc.calculate();
+		TrigoCalculate tc = new TrigoCalculate();
+		Quadratic qc = new Quadratic();
+		Rounding rc = new Rounding();
+		Menu m = new Menu();
+		boolean on = true;
+		int menu = 0;
+		Scanner reader = new Scanner(System.in);
+		
+		while (on) {
+			if (menu == 0) {
+				m.printMenu();
+				menu = Integer.parseInt(reader.nextLine());
+			}
+			switch (menu) {
+			case 1:
+				sc.calculate();
+				menu = 0;
+				break;
+			case 2:
+				tc.calculate();
+				menu = 0;
+				break;
+			case 3:
+				qc.quadraticCalculation();
+				menu = 0;
+				break;
+			case 4:
+				rc.rounding();
+				menu = 0;
+				break;
+			case 5:
+				System.out.println("Work in progress!");
+				System.out.println("Its alpha. Give us a break!");
+				menu = 0;
+				break;
+			case 6:
+				System.out.println("Disconected");
+				on = false;
+				break;
+			default:
+				System.out.println("Wrong Choise");
+				on = false;
+				break;
+			}
+		}
+		reader.close();
 	}
-
 }
--- src/Trigonometrija.java
+++ src/Trigonometrija.java
@@ -1,1 +1,29 @@
-//hahaha
+import java.util.Scanner;
+
+public class Trigonometrija {
+	Scanner reader = new Scanner(System.in);
+	
+	public void sin() {
+		System.out.println("Type a number:");
+		int sk = Integer.parseInt(reader.nextLine());
+		System.out.println("Number: " + sk + " SIN = " + Math.sin(sk));
+	}
+	
+	public void cos(){
+		System.out.println("Type a number:");
+		int sk = Integer.parseInt(reader.nextLine());
+		System.out.println("Number: " + sk + " COS = " + Math.cos(sk));
+	}
+	
+	public void tg(){
+		System.out.println("Type a number:");
+		int sk = Integer.parseInt(reader.nextLine());
+		System.out.println("Number: " + sk + " TG = " + Math.tan(sk));
+	}
+	
+	public void ctg(){
+		System.out.println("Type a number:");
+		int sk = Integer.parseInt(reader.nextLine());
+		System.out.println("Number: " + sk + " CTG = " + Math.cos(sk)/Math.sin(sk));
+	}
+}
